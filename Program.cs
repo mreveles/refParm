7fbdce00041d57aea2e21aeb7f908bd13e4528a8
mreveles <mareveles1@buffs.wtamu.edu>
2018-10-09 14:41:57 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace refParm
+{
+    class Program
+    {
+          public static void method(ref int refParm, int valParam){
+            refParm+=12;
+            valParam+=10;
+        }
+        static void Main(string [] args)
+        {
+            int i = 0, j=0;
+            method(ref i,j);
+            Console.WriteLine("i={0}, j={1}", i,j);
+        }
+    }
+}
